934ebb63986cbfca94cf89dc3cb87946544fa019
Nick-Giampetro <113528133+Nick-Giampetro@users.noreply.github.com>
2023-09-19 17:03:07 -0400

    added basic maths

--- AERSP450Project1.m
+++ AERSP450Project1.m
@@ -1,6 +1,16 @@
-longitude = -78 ;    % in degrees east negative for west
-latitude = 41 ;       % in degrees north negative for south
 
-T = 8 ;      % orbital period in hours
-T = T*3600 ;  % convert T to seconds
+
+mu = 3.986*10^5 ;    % mu of earth
+
+longitude = -78 ;    % in degrees east negative for west
+latitude = 41 ;      % in degrees north negative for south
+
+T = 8 ;         % orbital period in hours
+T = T*3600 ;    % convert T to seconds
+
+e = 0.5 ;       % e
+
+a = (mu*(T/(2*pi))^2)^(1/3)
+
+rp = a*(1-e)
 
