a1fa2742025fbf0269c0b8f5866e41d79cc54379
Nick-Giampetro <113528133+Nick-Giampetro@users.noreply.github.com>
2023-09-19 16:54:51 -0400

    worde

--- AERSP450Project1.m
+++ AERSP450Project1.m
@@ -1,4 +1,6 @@
-longitude = -78     % in degrees east negative for west
-latitude = 41       % in degrees north negative for south
+longitude = -78 ;    % in degrees east negative for west
+latitude = 41 ;       % in degrees north negative for south
 
-T = 8 % orbital period in hours
+T = 8 ;      % orbital period in hours
+T = T*3600 ;  % convert T to seconds
+
