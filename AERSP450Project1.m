6bd65b33991c77693821319cdbe29fbed274a3ba
Nick-Giampetro <113528133+Nick-Giampetro@users.noreply.github.com>
2023-09-20 01:22:14 -0400

    comments

--- AERSP450Project1.m
+++ AERSP450Project1.m
@@ -1,22 +1,32 @@
 
 
-mu = 3.986*10^5 ;    % mu of earth
+% ---- INPUT VALUES BELOW ---- %
 
+e = 0.5 ;       % eccentricity
+T = 8 ;         % orbital period in hours
+
+% ---- INPUT VALUES ABOVE ---- %
+
+% target location (state college)
 longitude = -78 ;    % in degrees east negative for west
 latitude = 41 ;      % in degrees north negative for south
 
-T = 8 ;         % orbital period in hours
+% constants
+lonAN = 60 ;         % cap-omega in degrees 
+mu = 3.986*10^5 ;    % mu of earth
+
+
+
 T = T*3600 ;    % convert T to seconds
 
-e = 0.5 ;       % e
 
-a = (mu*(T/(2*pi))^2)^(1/3)
+a = (mu*(T/(2*pi))^2)^(1/3) ;
 
-rp = a*(1-e)
+rp = a*(1-e) ;
 
 C1 = tand(longitude) ;
 C2 = tand(latitude)*sqrt(1+C1^2) ;
 
-R(1) = rp/sqrt(1+C1^2+C2^2);
-R(2) = R(1)*tand(longitude) ;
-R(3) = tand(latitude)*sqrt((R(1)^2+R(2)^2))
+R(1) = rp/sqrt(1+C1^2+C2^2) ;
+R(2) = R(1)*tand(longitude)  ;
+R(3) = tand(latitude)*sqrt((R(1)^2+R(2)^2)) ;
