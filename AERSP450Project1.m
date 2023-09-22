5d9731ccb1f58b5a2b65532f1530172d4d46e397
Nick-Giampetro <113528133+Nick-Giampetro@users.noreply.github.com>
2023-09-22 16:29:17 -0400

    commented out spaget

--- AERSP450Project1.m
+++ AERSP450Project1.m
@@ -4,8 +4,8 @@ close all ;
 
 % ---- INPUT VALUES BELOW ---- %
 
-e = 0.5 ;        % eccentricity
-T = 80 ;         % orbital period in hours
+e = 0.1;        % eccentricity
+T = 2 ;         % orbital period in hours
 
 % ---- INPUT VALUES ABOVE ---- %
 
@@ -34,23 +34,23 @@ R_ECI(1) = rp/sqrt(1+C1^2+C2^2) ;
 R_ECI(2) = R_ECI(1)*tand(longitude)  ;
 R_ECI(3) = tand(latitude)*sqrt((R_ECI(1)^2+R_ECI(2)^2)) 
 
-V_Perifocal(1) = sqrt(mu/p)*-sind(thetaPeriapsis) ;
-V_Perifocal(2) = sqrt(mu/p)*(e+cosd(thetaPeriapsis)) ;
-V_Perifocal(3) = 0 ;
-
-R_Perifocal(1) = rp*cosd(thetaPeriapsis) ;
-R_Perifocal(2) = rp*sind(thetaPeriapsis) ;
-R_Perifocal(3) = 0 
-
-h = cross(R_Perifocal,V_Perifocal) ;
-
-r = R_ECI
-
-I =  atand(-r(3)/(r(1)*sind(lonAN)-r(2)*cos(lonAN)))
-
-AoP = atand( (1/(r(1)+r(2)*tand(lonAN))) * ((r(3)*sind(I))/cosd(lonAN) + r(2)*cosd(I) - r(1)*cosd(I)*tand(lonAN)))
-
-cEP = dcm3axis(AoP)*dcm1axis(i)*dcm3axis(lonAN)
+% V_Perifocal(1) = sqrt(mu/p)*-sind(thetaPeriapsis) ;
+% V_Perifocal(2) = sqrt(mu/p)*(e+cosd(thetaPeriapsis)) ;
+% V_Perifocal(3) = 0 ;
+% 
+% R_Perifocal(1) = rp*cosd(thetaPeriapsis) ;
+% R_Perifocal(2) = rp*sind(thetaPeriapsis) ;
+% R_Perifocal(3) = 0 
+% 
+% h = cross(R_Perifocal,V_Perifocal) ;
+% 
+% r = R_ECI
+% 
+% I =  atand(-r(3)/(r(1)*sind(lonAN)-r(2)*cos(lonAN)))
+% 
+% AoP = atand( (1/(r(1)+r(2)*tand(lonAN))) * ((r(3)*sind(I))/cosd(lonAN) + r(2)*cosd(I) - r(1)*cosd(I)*tand(lonAN)))
+% 
+% cEP = dcm3axis(AoP)*dcm1axis(i)*dcm3axis(lonAN)
 
 % creates a dcm for an angle about axis 1
 function r = dcm1axis(ang)
