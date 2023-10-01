68e3caec35be3cea158f335ea32af0460972f7d9
Nick-Giampetro <113528133+Nick-Giampetro@users.noreply.github.com>
2023-10-01 14:19:14 -0400

    ode45

--- AERSP450Project1.m
+++ AERSP450Project1.m
@@ -41,7 +41,7 @@ V_Perifocal(3) = 0 ;
  
 R_Perifocal(1) = rp*cosd(thetaPeriapsis) ;
 R_Perifocal(2) = rp*sind(thetaPeriapsis) ;
-R_Perifocal(3) = 0 
+R_Perifocal(3) = 0 ;
 
 w = asind(R_ECEF(3)/(R_Perifocal(1)*sind(I)))
 
@@ -49,7 +49,7 @@ C1 = cosd(lonAN)/R_ECEF(2) + sind(lonAN)/R_ECEF(1) ;
 C2 = tand(lonAN)*cosd(lonAN)/R_ECEF(2) - sind(lonAN)/(R_ECEF(1)*tand(lonAN)) ;
 C3 = R_ECEF(1)/R_ECEF(2) + R_ECEF(2)/R_ECEF(1) ;
 
-GMST = acosd((R_Perifocal(1) * (cosd(w)*C1 - cosd(I)*sind(w)*C2))/C3)
+GMST = acosd((R_Perifocal(1) * (cosd(w)*C1 - cosd(I)*sind(w)*C2))/C3) 
 
 
 cEP = dcm3axis(w)*dcm1axis(I)*dcm3axis(lonAN) ;
@@ -64,7 +64,7 @@ options = odeset('reltol',1e-12,'abstol',1e-12);
 
 [t,R_ODE45] = ode45( @(t,R_ODE45) TwoBP(t,R_ODE45,mu) , t , init, options) ;
 
-grdTrck = groundTrack(t,R_ODE45,GMST,mu) ;
+grdTrck = groundTrack(t,R_ODE45,GMST) ;
 
 f = figure ;
 subplot(1,1,1)
@@ -81,11 +81,11 @@ exportgraphics(f,['X vs Z' '.jpg'])
 
 f = figure ;
 subplot(1,1,1)
-plot(grdTrck(:,1),grdTrck(:,2),'b')
+plot(grdTrck(:,1),grdTrck(:,2),'b',longitude,latitude,'X')
 exportgraphics(f,['long vs lat' '.jpg'])
 
 
-function dx = TwoBP(t,r,mu)
+function dx = TwoBP(~,r,mu)
     x = r(1) ;
     y = r(2) ;
     z = r(3) ;
@@ -103,17 +103,20 @@ function dx = TwoBP(t,r,mu)
 end
 
 
-function gt = groundTrack(t,r,GMST,mu)
+function gt = groundTrack(t,r,GMST)
+    r1 = zeros(length(t),3) ;
+    v1 = zeros(length(t),3) ;
 
+    
     for idx = 1:3
         r1(:,idx) = r(:,idx) ;
         v1(:,idx) = r(:,idx+3) ;
     end
 
-    %e_omega = 360/24/3600 ;
-    e_omega=0;
+    e_omega = 360/24/3600 ;
+    %e_omega = 0 ;
 
-    [r_ecef,v_ecef] = ECI2ECEF(r1,v1,e_omega,t,GMST,mu) ;
+    [r_ecef,~] = ECI2ECEF(r1,v1,e_omega,t,GMST) ;
     
     gt = zeros(length(t),2) ;
     phase = 0 ;
@@ -121,35 +124,39 @@ function gt = groundTrack(t,r,GMST,mu)
     for idx = 1:length(t)
         if mod(phase,3) == 0
             gt(idx,1) = atand(r_ecef(idx,2)/r_ecef(idx,1)) ;
-            gt(idx,2) = atand(r_ecef(idx,3)/sqrt(r_ecef(idx,1)^2+r_ecef(idx,2)^2)) ;
+            gt(idx,2) = atand( r_ecef(idx,3) / sqrt( r_ecef(idx,1)^2 + r_ecef(idx,2)^2 )) ;
         elseif  mod(phase,3) == 1
-            gt(idx,1) = -180 + atand(r_ecef(idx,2)/r_ecef(idx,1)) ;
+            gt(idx,1) = 180 + atand(r_ecef(idx,2)/r_ecef(idx,1)) ;
             gt(idx,2) = atand(r_ecef(idx,3)/sqrt(r_ecef(idx,1)^2+r_ecef(idx,2)^2)) ;
         elseif  mod(phase,3) == 2
-            gt(idx,1) = 180 + atand(r_ecef(idx,2)/r_ecef(idx,1)) ;
+            gt(idx,1) = -180 + atand(r_ecef(idx,2)/r_ecef(idx,1)) ;
             gt(idx,2) = atand(r_ecef(idx,3)/sqrt(r_ecef(idx,1)^2+r_ecef(idx,2)^2)) ;
         end
            
-        if (idx > 1) && ((gt(idx,1)-gt(idx-1,1) > 90 && (gt(idx,1)-gt(idx-1,1) < 350)))
+        if (idx > 1) && (gt(idx,1)-gt(idx-1,1) < -90 ) && (gt(idx-1) > 0)
            phase = phase + 1 ; 
-        elseif (idx > 1) && (gt(idx,1) < -180)
+           gt(idx,1) = 180 + atand(r_ecef(idx,2)/r_ecef(idx,1)) ;
+        elseif (idx > 1) && (gt(idx,1) > 180)
            phase = phase + 1 ;
-           gt(idx,1) = gt(idx,1) * -1 ;
+           gt(idx,1) = -180 + atand(r_ecef(idx,2)/r_ecef(idx,1)) ;
+        elseif (idx > 1) && (gt(idx,1)-gt(idx-1,1) < -90) && (gt(idx-1) < 0)
+           phase = phase + 1 ; 
+           gt(idx,1) = atand(r_ecef(idx,2)/r_ecef(idx,1)) ;
         end
         
 
     end
 end
 
-function [r_ecef,v_ecef] = ECI2ECEF(r_eci, v_eci, omega, t, GMST, mu)
+function [r_ecef,v_ecef] = ECI2ECEF(r_eci, v_eci, omega, t, GMST)
     r_ecef = zeros(length(t),3) ;
     v_ecef = zeros(length(t),3) ;
 
     for idx = 1 : length(t)
         gamma = GMST + t(idx) * omega ;
-        cECEF = dcm1axis(gamma) ;
-        r_ecef(idx,:) = cECEF * r_eci(idx,:)' ;
-        v_ecef(idx,:) = cECEF * v_eci(idx,:)' ;
+        cECEF = dcm3axis(gamma) ;
+        r_ecef(idx,:) = cECEF' * r_eci(idx,:)' ;
+        v_ecef(idx,:) = cECEF' * v_eci(idx,:)' ;
     end
 end
 
@@ -166,3 +173,18 @@ function r = dcm3axis(ang)
 r = [cosd(ang) sind(ang) 0 ; -sind(ang) cosd(ang) 0 ; 0 0 1];
 end
 
+function an = NewtonMethod(a,e,mu,t,tp,delta)
+
+M = sqrt(mu/(a^3))*(t-tp) ;
+error = delta + 1 ;
+Eo = M ;
+
+while error > delta
+    En = Eo - ((M+e*sin(Eo)-Eo)/(e*cos(Eo)-1)) ;
+    error = abs(En-Eo) ;
+    Eo = En ;
+end
+
+an = En;
+
+end
